e8afc39fb7d64acf34cb5177b912a5bb77011e80
dazeeeed <dazeeeed@gmail.com>
2022-05-09 22:55:17 +0200

    charts and climate data update

--- project_01/src/climatic_data.R
+++ project_01/src/climatic_data.R
@@ -5,9 +5,9 @@
 
 # Install pacman if not available and load packages
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(pacman, rio, here, ggplot2, dplyr)
+pacman::p_load(pacman, rio, here, ggplot2, dplyr, tidyr, stringr)
 
-
+here()
 # Data preparation
 data_path <- here("project_01", "data", "k_d_07_2021.csv")
 
@@ -27,21 +27,10 @@ data_cities <- data[data$station_name %in% cities, ]
 data_cities$station_name <- as.factor(data_cities$station_name)
 
 
-# data_city1 <- data[data$station_name == cities[1],]
-# data_city2 <- data[data$station_name == cities[2],]
-# data_city3 <- data[data$station_name == cities[3],]
-# 
-# boxplot(x=list(data_city1$max_daily_temp, 
-#         data_city2$max_daily_temp, 
-#         data_city3$max_daily_temp),
-#         names=cities,
-#         xlab="City", ylab="Temperature [\u00B0C]",
-#         main="Maximum daily temperature by city"
-# )
-
-plot(data_cities$station_name, data_cities$max_daily_temp)
-
-# Equivalent to plot
+###############################################################
+# Check differences between max_daily_temp for different cities
+###############################################################
+#plot(data_cities$station_name, data_cities$max_daily_temp)
 boxplot(data_cities$max_daily_temp ~ data_cities$station_name,
         xlab="City", ylab="Temperature [\u00B0C]",
         main="Maximum daily temperature by city"
@@ -55,6 +44,9 @@ additional_stats<- data_cities %>%
 
 additional_stats
 
+###############################################################
+# Check daily temperature fluctuations (day-night)
+###############################################################
 data_cities$daily_temp_fluctuation <- data_cities$max_daily_temp - 
   data_cities$min_daily_temp
 
@@ -64,9 +56,61 @@ boxplot(data_cities$daily_temp_fluctuation ~ data_cities$station_name,
         main="Daily temperature fluctuation by city"
 )
 
+###############################################################
+# Check how does max_daily_temp change day by day
+###############################################################
+data_cities <- data_cities %>% 
+  group_by(station_name) %>%
+  mutate(max_daily_temp_day_change = max_daily_temp - lag(max_daily_temp, 1))
+
+data_cities$max_daily_temp_day_change <- tidyr::replace_na(
+  data_cities$max_daily_temp_day_change, 0)
+
+
+boxplot(data_cities$max_daily_temp_day_change ~ data_cities$station_name,
+        xlab="City", ylab="Temperature [\u00B0C]",
+        main="Day by day max temperature fluctuation by city"
+)
+
+# Put graphs in 3 rows and 1 column
+par(mfrow = c(3, 1))
+x_range <- c(-10, 10)
+hist_breaks <- seq(from=-10, to=10, by=1)
+colors <- c("red", "purple", "blue")
+plot_scale_factor <- 1
+
+# Histograms for each species using options
+# ?pdf
+# pdf(file="freq_max_daily_temp_change.pdf", 
+#     width = 7, 
+#     height = 7, 
+#     units="in", 
+#     noRStudioGD = TRUE
+# )
+
+for(i in 1:length(cities)){
+  hist(data_cities$max_daily_temp_day_change[data_cities$station_name == 
+                                               cities[i]],
+       xlim = x_range,
+       breaks = hist_breaks,
+       main = paste("Frequency of day by day change in max daily temperature", 
+                    "in", str_to_sentence(cities[i])),
+       xlab = "Temperature difference [\u00B0C]",
+       col = colors[i],
+       cex.lab=plot_scale_factor, 
+       cex.axis=plot_scale_factor, 
+       cex.main=plot_scale_factor, 
+       cex.sub=plot_scale_factor)
+}
+dev.off()
+
+# Restore graphic parameter
+par(mfrow=c(1, 1))
+rm(x_range, hist_breaks)
+
 
 ?median
-
+?rm
 
 # CLEANUP
 # Clear environment
